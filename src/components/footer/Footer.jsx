759621acd6dfbcff705220f324eb35e218f34962
Janaina-Costa <janainacosta.dev@gmail.com>
2022-02-01 22:27:42 -0300

    acerto2

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 import linkedin from '../img/likdin.png'
 import git from '../img/github.png'
 import './Footer.css'
@@ -8,10 +9,10 @@ const Footer = ()=>{
         <footer>
                 <h4 className='marca'> Janaina Costa</h4>
                 
-               <a href="https://www.linkedin.com/in/janaina-costa-1193241b2/" ><img className='redesocial' width='40px' height='40px' src={linkedin} alt="linkedin img" /></a> 
+               <a href="https://www.linkedin.com/in/janaina-costa-1193241b2/" target='_blank'><img className='redesocial' width='40px' height='40px' src={linkedin} alt="linkedin img" /></a> 
 
 
-                <a href="https://github.com/Janaina-Costa" ><img className='redesocial' width='40px' height='40px' src={git}alt="github img" /></a> 
+                <a href="https://github.com/Janaina-Costa" target='_blank'><img className='redesocial' width='40px' height='40px' src={git}alt="github img" /></a> 
                 
             </footer>
 
